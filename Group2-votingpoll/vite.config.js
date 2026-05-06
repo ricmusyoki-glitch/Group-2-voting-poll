25592e84f35403ac6f5fbdebe2d3475af8176f09
Ric Musyoki <ric.musyoki@student.moringaschool.com>
2026-05-06 12:41:56 +0300

    Initial commit

---
+++ Group2-votingpoll/vite.config.js
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vite'
+import react, { reactCompilerPreset } from '@vitejs/plugin-react'
+import babel from '@rolldown/plugin-babel'
+import tailwind from 'tailwindcss'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    tailwind(),
+
+    babel({ presets: [reactCompilerPreset()] })
+  ],
+})
